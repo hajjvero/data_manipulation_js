4e8bd9833951113d7ce10593d0298ac4aa4f45f3
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2025-11-21 10:22:33 +0100

    feat: exercice 2

--- Exercice/ex-1/app.js
+++ Exercice/ex-1/app.js
@@ -1,5 +1,54 @@
 import {employees} from '../../dataset.js';
 
+const employeeForm = document.getElementById('employee-form');
+
+const inputFirstName = document.getElementById('input-firstName');
+
+const inputLastName = document.getElementById('input-lastName');
+
+const inputAge = document.getElementById('input-age');
+
+const inputDepartment = document.getElementById('input-department');
+
+const skillsContainer = document.getElementById('skills-container');
+
+const addSkillBtn = document.getElementById('add-skill-btn');
+
+addSkillBtn.addEventListener('click', () => {
+    const inp = document.createElement('input');
+    inp.className='skils-inputs';
+    inp.setAttribute('type', 'text');
+    inp.setAttribute('required', 'true');
+
+    skillsContainer.append(inp);
+})
+
+employeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputsSkilles = Array.from(document.getElementsByClassName('skils-inputs'));
+    const emp = {
+        id: employees.length + 1,
+        firstName: inputFirstName.value,
+        lastName: inputLastName.value,
+        age: inputAge.value,
+        department: inputDepartment.value,
+        skills: inputsSkilles.map((inp) => inp.value),
+    };
+
+    employees.push(emp);
+
+    clearForm();
+    showList();
+});
+
+
+function clearForm() {
+    inputFirstName.value = '';
+    inputLastName.value = '';
+    inputAge.value = '';
+    skillsContainer.innerHTML = '';
+}
+
 function toggleContainer() {
     const container = document.getElementById("employees-container");
     const btn = document.getElementById("btn");
@@ -23,12 +72,12 @@ function showList() {
               <td>${e.department}</td>
              <td>${e.position}</td>
               <td>${e.salary}</td>
-             <td>${e.skills.join(',')}</td>
+             <td>${e.skills?.join(',')}</td>
              <td>${e.joinDate}</td>
              <td>${e.email}</td>
-             <td>${e.projects.join(',')}</td>
+             <td>${e.projects?.join(',')}</td>
              <td>${e.isActive ? 'Yes' : 'No'}</td>
-             <td>${e.languages.join(',')}</td>
+             <td>${e.languages?.join(',')}</td>
         `;
 
         employeesTbody.append(tr);
